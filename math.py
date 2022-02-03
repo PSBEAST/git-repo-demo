625831533ee7be660866bb4255f875bb44cf9c38
PSBEAST <69676198+PSBEAST@users.noreply.github.com>
2022-02-03 20:23:40 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#addition implementation
+def add(x,y):
+    return x+y                    # on Bug123
+#subtraction implementation
+def sub(x,y):
+    return x-y                    #on master
+#multiplication implementation
+def mul(x,y):
+    return x*y                     # on Bug456
+# division implementation
+def div(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR    # on Bug789
+    else:
+        return x/y
+
